7b01d7b3464b8f87a420ac0b5d840d1caca6ec99
João Victor <albjoaov@gmail.com>
2019-05-02 21:05:53 -0300

    first commit

---
+++ src/main/java/com/hivecloud/pedidos/domain/Pedidos.java
@@ -0,0 +1,9 @@
+package com.hivecloud.pedidos.domain;
+
+import java.util.List;
+
+public class Pedidos {
+
+	private List<Pratos> pedidos;
+
+}
---
+++ src/main/java/com/hivecloud/pedidos/domain/Pratos.java
@@ -0,0 +1,12 @@
+package com.hivecloud.pedidos.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Pratos {
+
+	private String nome;
+	private List<String> acompanhamentos;
+	private BigDecimal preco;
+
+}
