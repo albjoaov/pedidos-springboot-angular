3b059c1b8182fe3af368159babfa26e0eaaa35ad
João Victor <albjoaov@gmail.com>
2019-05-15 01:45:09 -0300

    Handle objects at dropdown

--- Front-end/my-app/src/app/Components/Pedido/add-pedido/add-pedido.component.ts
+++ Front-end/my-app/src/app/Components/Pedido/add-pedido/add-pedido.component.ts
@@ -23,6 +23,7 @@ export class AddPedidoComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private pratoService: PratoService, private pedidoService: PedidoService, private router: Router) { }
 
   ngOnInit() {
+    this.populaDropdownList();
     this.populaInputs();
     this.loadMultiSelectData();
     this.form = this.formBuilder.group({
@@ -32,33 +33,58 @@ export class AddPedidoComponent implements OnInit {
     })
   }
 
-  loadMultiSelectData (){
-    this.dropdownList = [
-      { item_id: 1, item_text: 'Mumbai' },
-      { item_id: 2, item_text: 'Bangaluru' },
-      { item_id: 3, item_text: 'Pune' },
-      { item_id: 4, item_text: 'Navsari' },
-      { item_id: 5, item_text: 'New Delhi' }
-    ];
-    this.selectedItems = [
-      { item_id: 3, item_text: 'Pune' },
-      { item_id: 4, item_text: 'Navsari' }
-    ];
+  loadMultiSelectData() {
+    this.dropdownList = []
+    //   { text: 'Mumbai' },
+    //   { text: 'Bangaluru' },
+    //   { text: 'Pune' },
+    //   { text: 'Navsari' },
+    //   { text: 'New Delhi' }
+    // ];
+    this.selectedItems = [];
     this.dropdownSettings = {
       singleSelection: false,
-      idField: 'item_id',
-      textField: 'item_text',
+      idField: 'id',
+      textField: 'text',
       selectAllText: 'Selecionar todos',
       unSelectAllText: 'Desmarcar todos',
       itemsShowLimit: 5,
       allowSearchFilter: true,
-      searchPlaceholderText	: "Digite aqui para filtrar",
-      limitSelection: 4,
+      searchPlaceholderText: "Digite aqui para filtrar",
+      limitSelection: 99,
     };
   }
 
+  populaDropdownList() {
+    var listaPratos = []
+    const meuSet = new Set();
+
+    this.pratoService.getPratos()
+      .subscribe(data => {
+        listaPratos = data;
+
+        listaPratos.forEach(element => {
+
+          for (var key in element) {
+            if (element.hasOwnProperty(key) && key == 'acompanhamentos') {
+              element[key].forEach(acompanhamento => {
+                meuSet.add(acompanhamento);
+              });
+            }
+          }
+        });
+        this.dropdownList = Array.from(meuSet);
+      });
+
+  }
+
   onItemSelect(item: any) {
-    console.log(item);
+    this.selectedItems.push(item)
+  }
+
+  onItemDeSelect(item: any) {
+    const valueToRemove = item.id
+    this.selectedItems = this.selectedItems.filter(item => { return item.id != valueToRemove })
   }
 
 
@@ -71,10 +97,13 @@ export class AddPedidoComponent implements OnInit {
   addPedido() {
     const pratos: Prato[] = []
     const pedido: Pedido = new Pedido(pratos);
-    
+
+
     const novoPrato = this.form.value
-    //const acompanhamentosList = this.form.value.acompanhamentos.split(',');
-    //novoPrato.acompanhamentos = acompanhamentosList
+
+    console.log(this.selectedItems);
+    const acompanhamentosList = this.form.value.acompanhamentos.split(',');
+    novoPrato.acompanhamentos = acompanhamentosList
 
     pedido["pratos"].push(novoPrato)
     this.pedidoService.createPedido(pedido)
